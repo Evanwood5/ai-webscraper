fe5c26ae7bc2476fd5a5b62c7c2f23efefe32dce
Evanwood5 <evan.wood861@gmail.com>
2025-05-24 19:01:37 -0400

    Fix: updated install_chrome.sh for Render compatibility

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Install Google Chrome
+# Download Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get update
-apt-get install -y ./google-chrome-stable_current_amd64.deb
 
-# Get matching ChromeDriver version
-CHROME_VERSION=$(google-chrome --version | cut -d ' ' -f3 | cut -d '.' -f1)
-wget -N https://chromedriver.storage.googleapis.com/$CHROME_VERSION/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-chmod +x chromedriver
-mv -f chromedriver /usr/local/bin/chromedriver
+# Extract it (Render doesn't support installing via apt)
+dpkg-deb -x google-chrome-stable_current_amd64.deb $HOME/chrome
+
+# Set CHROME path for Selenium
+export CHROME_BIN=$HOME/chrome/opt/google/chrome/google-chrome
+
+# Download ChromeDriver (match your local Chrome version, e.g., 124)
+wget https://storage.googleapis.com/chrome-for-testing-public/124.0.6367.91/linux64/chromedriver-linux64.zip
+unzip chromedriver-linux64.zip
+mv chromedriver-linux64/chromedriver $HOME/chromedriver
+chmod +x $HOME/chromedriver
+
+# Add to PATH
+export PATH=$HOME:$PATH
